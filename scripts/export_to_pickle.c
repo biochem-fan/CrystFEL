627491e3fac3bea1c9ba41eb8cb6546a11e64358
Takanori Nakane <nakane.t@gmail.com>
2014-08-19 11:46:08 +0100

    Added scripts/export_to_py to export observations in a stream file to pickle files cctbx.xfel can read. (written to test cxi.postrefine)
    WARNING: This is not working well. Can be a bug in this converter,
             or incompatibility in sigma, or something else.

---
+++ scripts/export_to_pickle.c
@@ -0,0 +1,86 @@
+/* export_to_pickle
+
+   export observations in a stream file to pickle files
+   cctbx.xfel can read.
+
+   WARNING: This program is NOT tested throughfully and NOT working well.
+
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "stream.h"
+#include "image.h"
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    printf("Wrong number of arguments.\n");
+    printf("Usage: export_to_pickle geometry.geom input.stream | cctbx.python export_to_pickle.py\n");
+    return -1;
+  }
+  struct detector *geom = get_detector_geometry(argv[1]);
+  Stream *st = open_stream_for_read(argv[2]);
+  
+  if (geom == NULL) {
+    printf("Failed to open geometry %s.\n", argv[1]);
+    return -1;
+  }
+  if (st == NULL) {
+    printf("Failed to open stream %s.\n", argv[2]);
+    return -1;
+  }
+
+  struct image image;
+  image.det = geom;
+
+  int ncryst = 0, j = 0;
+  while (!read_chunk(st, &image)) {
+    for (j = 0; j<image.n_crystals; j++ ) {
+      Crystal *cr = image.crystals[j];
+      ncryst++;
+      printf("======================== Crystal %d\n", ncryst);
+      printf("%f\n", image.lambda * 10E9); // in A
+      double asx, asy, asz;
+      double bsx, bsy, bsz;
+      double csx, csy, csz;
+      cell_get_reciprocal(crystal_get_cell(cr),
+			  &asx, &asy, &asz,
+			  &bsx, &bsy, &bsz,
+			  &csx, &csy, &csz);
+      printf("%f %f %f %f %f %f %f %f %f\n", asx, asy, asz, bsx, bsy, bsz, csx, csy, csz);
+      printf("%d\n", num_reflections(crystal_get_reflections(cr)));      
+
+      Reflection *refl;
+      RefListIterator *iter;      
+      for (refl = first_refl(crystal_get_reflections(cr), &iter);
+	   refl != NULL;
+	   refl = next_refl(refl, iter)) {
+	
+	signed int h, k, l;
+	double fs, ss, xs, ys, x, y;
+	struct panel *p;
+
+	get_indices(refl, &h, &k, &l);
+	get_detector_pos(refl, &fs, &ss);
+	
+	p = find_panel(geom, fs, ss);
+	if (p == NULL) continue;
+	
+	xs = (fs-p->min_fs)*p->fsx + (ss-p->min_ss)*p->ssx;
+	ys = (fs-p->min_fs)*p->fsy + (ss-p->min_ss)*p->ssy;
+	x = xs + p->cnx;
+	y = ys + p->cny;
+	
+	printf("%i %i %i %f %f %f %f\n", h, k, l, 
+	       get_intensity(refl), get_esd_intensity(refl), x, y);
+      }			
+      
+      reflist_free(crystal_get_reflections(cr));
+      cell_free(crystal_get_cell(cr));
+      crystal_free(cr);		  
+    }
+  }
+ 
+  return 0;
+}
---
+++ scripts/export_to_pickle.py
@@ -0,0 +1,83 @@
+'''
+   export_to_pickle
+
+   export observations in a stream file to pickle files
+   cctbx.xfel can read.
+
+   WARNING: This program is NOT tested throughfully and NOT working well.
+
+'''
+
+from cctbx.array_family import flex
+from cctbx import miller, crystal, sgtbx, uctbx, crystal_orientation
+from scitbx import matrix
+import cPickle as pickle
+import sys
+
+# reference http://cctbx.sourceforge.net/siena2005/sort_merge.py
+
+# This will be overwritten by values in .phil but still you need it!
+# This field in pickle is named 'pointgroup' but actually seems to be space group...
+Pointgroup = "P43212"
+
+i_cryst = 0
+
+def nextline():
+    return sys.stdin.readline()
+
+while (True):
+    inp = nextline().strip()
+    if (inp == ""):
+        break
+    print inp
+    i_cryst += 1
+
+    inp = nextline()
+    wavelength = float(inp)
+    tmp = [float(x) * 1E-10 for x in nextline().split()]
+
+    #    flip x and y-axis
+    #    tmp[0], tmp[3], tmp[6], tmp[1], tmp[4], tmp[7] = tmp[1], tmp[4], tmp[7], tmp[0], tmp[3], tmp[6], 
+
+    # flip the z-axis
+    # FIXME: Check if this is correct.
+    tmp[2] *= -1
+    tmp[5] *= -1
+    tmp[8] *= -1
+
+    reciprocal_basis = matrix.sqr(tmp).transpose()
+    current_orientation = crystal_orientation.crystal_orientation(reciprocal_basis, 
+                                                                  crystal_orientation.basis_type.reciprocal)
+    n_reflections = int(nextline())
+    
+    data = flex.double()
+    sigmas = flex.double()
+    miller_indices = flex.miller_index()
+    mapped_predictions = flex.vec2_double()
+    
+    for i in xrange(0, n_reflections):
+        fields = nextline().split()
+        miller_indices.append([int(value) for value in fields[:3]])
+        data.append(float(fields[3]))
+        sigmas.append(float(fields[4]))
+        mapped_predictions.append([float(fields[5]), float(fields[6])])
+    
+    point_group = Pointgroup
+    crystal_symmetry = crystal.symmetry(unit_cell=current_orientation.unit_cell(),
+                                        space_group = sgtbx.space_group())
+        
+    miller_set = miller.set(crystal_symmetry=crystal_symmetry,
+                            indices=miller_indices,
+                            anomalous_flag=False)
+    observations = miller.array(miller_set, data=data,
+                                sigmas=sigmas)
+        
+    save_to_pickle = {'xbeam': 0, 'ybeam': 0, 'wavelength': wavelength,
+                      'pointgroup': point_group,
+                      'mapped_predictions': [mapped_predictions],
+                      'observations': [observations],
+                      'current_orientation': [current_orientation]}
+        
+    pickle.dump(save_to_pickle, open("%d.pickle" % i_cryst, "wb"))
+
+
