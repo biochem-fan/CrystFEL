biochem-fan/CrystFEL

commit e6d601bb47860e4071735b110d07394d89d18da7

More colors for multiple-lattice

src/dw-hdfsee.c
@@ -497,6 +497,12 @@ static int draw_stuff(cairo_surface_t *surf, DisplayWindow *dw)
 				cairo_set_source_rgb(cr, 0.0, 1.0, 0.0); 
 			} else if (f->intensity == FEATURE_PREDICTION + 2) { // third lattice
 				cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
+			} else if (f->intensity == FEATURE_PREDICTION + 3) { // forth lattice
+				cairo_set_source_rgb(cr, 0.0, 1.0, 1.0);
+			} else if (f->intensity == FEATURE_PREDICTION + 4) { // fifth lattice
+				cairo_set_source_rgb(cr, 1.0, 0.0, 1.0);
+			} else if (f->intensity >= FEATURE_PREDICTION + 5) { // > sixth lattice
+				cairo_set_source_rgb(cr, 0.5, 1.0, 1.0);
 			} else {
 				switch ( dw->scale ) {
 					case SCALE_COLOUR :

commit 418ff0def70ce934c1d0880a707d504651a5ee68

Stop hijacking imagefeature.intensity to fix a bug introduced in commit e6d601bb47860e4071735b110d07394d89d18da7.

doc/reference/libcrystfel/xml/coding-standards.xml
@@ -1,157 +1,0 @@
-<?xml version="1.0"?>
-<!DOCTYPE refentry PUBLIC "-//OASIS//DTD DocBook XML V4.3//EN"
-               "http://www.oasis-open.org/docbook/xml/4.3/docbookx.dtd"
-[
-  <!ENTITY % local.common.attrib "xmlns:xi  CDATA  #FIXED 'http://www.w3.org/2003/XInclude'">
-]>
-<refentry id="CrystFEL-coding-standards">
-<refmeta>
-<refentrytitle role="top_of_page" id="CrystFEL-coding-standards.top_of_page">CrystFEL coding standards</refentrytitle>
-<manvolnum>3</manvolnum>
-<refmiscinfo>
-  Coding  standards
-</refmiscinfo>
-</refmeta>
-<refnamediv></refnamediv>
-
-
-<refsect1 id="CrystFEL-coding-standards.description" role="desc">
-<title role="desc.title">Summary</title>
-<para>
-This page documents the coding conventions used within the CrystFEL source code.  Read these to help when reading the code or before making modifications destined to be sent upstream.
-</para>
-</refsect1>
-
-
-<refsect1 id="CrystFEL-coding-standards.license">
-<title>Licensing and copyright</title>
-<para>
-CrystFEL is distributed under the terms of the GNU General Public License version 3 or higher.  Contributions are very welcome provided they also use this license.
-</para>
-<para>
-Whenever you edit a source file, don't forget to update the copyright dates at the top.  Add your name and email address if they're not there already.  Be sure to add your name to the 'AUTHORS' file in the top level folder, as well.
-</para>
-</refsect1>
-
-
-<refsect1 id="CrystFEL-coding-standards.formatting">
-<title>Formatting</title>
-<para>
-<emphasis>Indentation</emphasis> is done with <emphasis>tabs</emphasis> and
-<emphasis>alignment</emphasis> is done with spaces.  This way, the code looks
-neat whatever width you configure your editor to display tabs as.  This means,
-for example:
-</para>
-<para>
-<programlisting>
-struct something
-{
-	int thing;                   /* &lt;--- spaces used to align comments */
-	int thing_with_longer_name;  /* &lt;--- spaces used to align comments */
-}
-
-void somefunction(int something)
-{
-	/* &lt;--- Tab character used at the start of this line */
-}
-</programlisting>
-</para>
-<para>
-However, code must be <emphasis>strictly</emphasis> wrapped at 80 columns, or
-what would be 80 columns if the tabs were displayed as 8 spaces.
-If you think you need more width, you're at too many levels of indentation and
-need to break things down a bit.  There are no exceptions whatsoever.
-</para>
-<para>
-When performing a two or three dimensional iteration, for example over image
-coordinates or Miller indices, it is acceptable to indent as follows:
-</para>
-<para>
-<programlisting>
-for ( h=-10; h&lt;+10; h++ ) {
-for ( k=-10; k&lt;+10; k++ ) {
-for ( l=-10; l&lt;+10; l++ ) {
-
-	/* Do stuff */
-
-}
-}
-}
-</programlisting>
-</para>
-</refsect1>
-
-
-<refsect1 id="CrystFEL-coding-standards.brackets">
-<title>Brackets and so on</title>
-<para>
-Brackets and so on should go like this:
-</para>
-<para>
-<programlisting>
-/* Multiple line comments have stars
- * down one side */
-void somefunction(int someparam)
-{
-	/* Single line comments use this style (not //) */
-	if ( a &lt; b ) {
-		/* 'if' statements usually have the opening brace on the same
-		 * line as the condition. */
-	} else {
-		/* 'else's are 'cuddled' */
-	}
-
-	if ( some &amp;&amp; very &amp;&amp; long &amp;&amp; condition &amp;&amp; that &amp;&amp; spans
-	     &amp;&amp; two &amp;&amp; lines )
-	{
-		/* Opening brace is on a line by itself in the case of a very
-		 * long condition */
-	}
-}
-
-/* Comments use proper capitalisation to make things look neat */
-</programlisting>
-</para>
-<para>'struct' blocks can have the braces like functions or 'if' statements.  Usually the former looks nicer if the struct is large.</para>
-<para>Parentheses should have spaces after them in 'if' statements, but not in function calls. Function arguments should have spaces after the comma.  There should be no space between the function name and the opening bracket.  That means:</para>
-<para><programlisting>
-if ( something ) {
-	do_something(a, b, c);
-}
-</programlisting></para>
-<para>instead of:</para>
-<para><programlisting>
-if (something) {
-	do_something (a,b,c);
-}
-</programlisting></para>
-</refsect1>
-
-
-<refsect1 id="CrystFEL-coding-standards.cleverness">
-<title>Cleverness</title>
-<para>
-Yes, we all know you can insert a new node into an RB-tree while simultaneously
-calculating Pi to 150 decimal places in one line of code.  You don't need to
-prove it here.  As a general rule, if you think you're about to do something clever, <emphasis>don't do it at all</emphasis>.
-</para>
-</refsect1>
-
-
-<refsect1 id="CrystFEL-coding-standards.commitmessages">
-<title>VCS commit messages</title>
-<para>The first line of your commit message should include a one line summary of the changes, in the form "Do XYZ".  That is, not "Did XYZ".</para>
-<para>Make the minimum possible changes in each commit.  Try to really distill your changes down to the bare bones, and keep 'cleaning up' in separate commits.  Remember that Git thinks about 'changes' rather than 'versions'.</para>
-</refsect1>
-
-
-<refsect1 id="CrystFEL-coding-standards.evilness">
-<title>Evil dictator</title>
-<para>
-Despite your following all of the above, I will probably still touch up your
-code in some places while (or shortly after) integrating it into mainline
-CrystFEL.  Please try not to take it personally.
-</para>
-</refsect1>
-
-</refentry>

src/dw-hdfsee.c
@@ -492,17 +492,18 @@ static int draw_stuff(cairo_surface_t *surf, DisplayWindow *dw)
 
 			cairo_arc(cr, x/dw->binning, y/dw->binning,
 				  radius, 0.0, 2.0*M_PI);
-			if (f->intensity == FEATURE_PREDICTION + 0) { /* first lattice */
+			printf("%f\n", f->rx);
+			if (f->rx == FEATURE_PREDICTION + 0) { /* first lattice */
 				cairo_set_source_rgb(cr, 1.0, 0.0, 0.0);
-			} else if (f->intensity == FEATURE_PREDICTION + 1) { /* second lattice */
+			} else if (f->rx == FEATURE_PREDICTION + 1) { /* second lattice */
 				cairo_set_source_rgb(cr, 0.0, 1.0, 0.0); 
-			} else if (f->intensity == FEATURE_PREDICTION + 2) { /* third lattice */
+			} else if (f->rx == FEATURE_PREDICTION + 2) { /* third lattice */
 				cairo_set_source_rgb(cr, 0.0, 0.0, 1.0);
-			} else if (f->intensity == FEATURE_PREDICTION + 3) { /* forth lattice */
+			} else if (f->rx == FEATURE_PREDICTION + 3) { /* forth lattice */
 				cairo_set_source_rgb(cr, 0.0, 1.0, 1.0);
-			} else if (f->intensity == FEATURE_PREDICTION + 4) { /* fifth lattice */
+			} else if (f->rx == FEATURE_PREDICTION + 4) { /* fifth lattice */
 				cairo_set_source_rgb(cr, 1.0, 0.0, 1.0);
-			} else if (f->intensity >= FEATURE_PREDICTION + 5) { /* > sixth lattice */
+			} else if (f->rx >= FEATURE_PREDICTION + 5) { /* > sixth lattice */
 				cairo_set_source_rgb(cr, 0.5, 1.0, 1.0);
 			} else {
 				switch ( dw->scale ) {
@@ -1352,13 +1353,14 @@ static void stream_selection_changed(GtkTreeSelection *selection, DisplayWindow 
 		/* load spot list */
 		if (dw->stream != NULL && seek_stream(dw->stream, offset) == 0) {
 			int rval, j;
+			struct imagefeature *feature;
       
 			rval = read_chunk(dw->stream, dw->image);
 			if (rval) {
 				printf("Failed to load stream for image %s \n", dw->image->filename);
 			} else {
 				for (j = 0; j < dw->image->num_peaks; j++) {
-					struct imagefeature *feature = image_get_feature(dw->image->features, j);
+					feature = image_get_feature(dw->image->features, j);
 					if (feature == NULL) continue;
 					feature->rx = FEATURE_SPOT; /* FIXME: reciprocal_x field is hijacked! */
 					if (feature->name == NULL) feature->name = strdup("Spot");
@@ -1383,9 +1385,10 @@ static void stream_selection_changed(GtkTreeSelection *selection, DisplayWindow 
 						char name[64];
 						snprintf(name, 63, "(%i, %i, %i) on lattice %d", h, k, l, j);
 
-						/* FIXME: intensity field is hijacked! */
-						image_add_feature(dw->image->features, fs, ss, dw->image, j + FEATURE_PREDICTION,
-						strdup(name));
+						image_add_feature(dw->image->features, fs, ss, dw->image, 0, strdup(name));
+						feature = image_get_feature(dw->image->features, dw->image->num_peaks);
+						dw->image->num_peaks++; /* or, should we use a local variable? */
+						feature->rx = FEATURE_PREDICTION + j;
 					}			
 	  
 					reflist_free(crystal_get_reflections(cr));